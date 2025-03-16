83aedb54aed25e13b3d94f188efb3c67cce33c49
Omarraafat2 <omarraafat476@gmail.com>
2025-03-16 19:11:16 +0200

    Task is Finshed

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
-// import { ProductNameTypes } from "../types";
+import { ProductNameTypes } from "../types";
 
 export interface IProduct {
   id?: string;
@@ -15,13 +15,13 @@ export interface IProduct {
 
 export interface IFormInput {
   id: string;
-  name: string;
+  name: ProductNameTypes;
   label: string;
   type: string;
 }
 
-// export interface ICategory {
-//   id: string;
-//   name: string;
-//   imageURL: string;
-// }
+export interface ICategory {
+  id: string;
+  name: string;
+  imageURL: string;
+}
